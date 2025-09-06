f0db2f924773354f84ca14864f28957c41e16dfe
KryptoKomal <kryp70girl@gmail.com>
2025-09-06 23:38:58 +0530

    Create Ques12.java

---
+++ Level1_Control_flow/Ques12.java
@@ -0,0 +1,26 @@
+package Level1_control_flow;
+
+import java.util.Scanner;
+
+public class ques12 {
+   public ques12() {
+   }
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      if (n >= 0) {
+         int sumFormula = n * (n + 1) / 2;
+         int sumLoop = 0;
+
+         for(int i = 1; i <= n; ++i) {
+            sumLoop += i;
+         }
+
+         System.out.println("Formula: " + sumFormula + " Loop: " + sumLoop);
+      } else {
+         System.out.println("Not a natural number");
+      }
+
+   }
+}
