ec05b59714197f47114d5ef12cb666a652cc1d6f
KryptoKomal <kryp70girl@gmail.com>
2025-09-06 22:50:16 +0530

    Create Ques2.java

---
+++ Level2_java_element/Ques2.java
@@ -0,0 +1,19 @@
+package Level2_java_element;
+
+import java.util.Scanner;
+
+public class ques2 {
+   public ques2() {
+   }
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter base (cm): ");
+      double base = sc.nextDouble();
+      System.out.print("Enter height (cm): ");
+      double height = sc.nextDouble();
+      double areaCm = 0.5 * base * height;
+      double areaIn = areaCm / 6.4516;
+      System.out.println("The Area of the triangle in sq in is " + areaIn + " and sq cm is " + areaCm);
+   }
+}
