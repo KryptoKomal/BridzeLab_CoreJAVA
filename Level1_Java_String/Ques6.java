01847065b1dfc0b58ce596adb18644c7cf9eb1f0
KryptoKomal <kryp70girl@gmail.com>
2025-09-08 11:21:10 +0530

    Create Ques6.java

---
+++ Level1_Java_String/Ques6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IllegalArgumentDemo {
+    static void generateException(String s) {
+        System.out.println(s.substring(5, 2));
+    }
+
+    static void handleException(String s) {
+        try {
+            System.out.println(s.substring(5, 2));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Handled: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+   
+        handleException(s);
+        sc.close();
+    }
+}
