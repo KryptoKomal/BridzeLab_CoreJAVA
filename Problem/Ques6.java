f4d4e5b65888bca88d4c8251fdaee277f7600008
KryptoKomal <kryp70girl@gmail.com>
2025-09-08 13:40:38 +0530

    Create Ques6.java

---
+++ Problem/Ques6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter principal: ");
+        double principal = sc.nextDouble();
+        System.out.print("Enter rate (%): ");
+        double rate = sc.nextDouble();
+        System.out.print("Enter time (years): ");
+        double time = sc.nextDouble();
+        double interest = (principal * rate * time) / 100;
+        System.out.println("Simple Interest: " + interest);
+    }
+}
