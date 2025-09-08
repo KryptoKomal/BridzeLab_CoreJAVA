15345da352d42b31625ea87ec59cd9ff594a3137
KryptoKomal <kryp70girl@gmail.com>
2025-09-08 12:49:12 +0530

    Create Ques9.java

---
+++ Level3_Java_Method/Ques9.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class CalendarDisplay {
+    static String[] months = {
+        "", "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    };
+
+    static int[] daysInMonth = {
+        0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    };
+
+    static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    static int getNumberOfDays(int month, int year) {
+        if (month == 2 && isLeapYear(year)) return 29;
+        return daysInMonth[month];
+    }
+
+    static int getFirstDay(int month, int year) {
+        int d = 1;
+        int y0 = year - (14 - month) / 12;
+        int x = y0 + y0/4 - y0/100 + y0/400;
+        int m0 = month + 12 * ((14 - month) / 12) - 2;
+        return (d + x + (31*m0)/12) % 7;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int year = sc.nextInt();
+        sc.close();
+
+        System.out.println("     " + months[month] + " " + year);
+        System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+
+        int firstDay = getFirstDay(month, year);
+        int numberOfDays = getNumberOfDays(month, year);
+
+        for (int i = 0; i < firstDay; i++) System.out.print("    ");
+
+        for (int day = 1; day <= numberOfDays; day++) {
+            System.out.printf("%3d ", day);
+            if ((day + firstDay) % 7 == 0) System.out.println();
+        }
+        System.out.println();
+    }
+}
